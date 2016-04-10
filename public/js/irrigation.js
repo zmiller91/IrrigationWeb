e9c34a82bef354103837afc5296e08251b356f7c
unknown <2.4.8.16.32.sixtyfour@gmail.com>
2016-04-10 10:25:06 -0600

    made some changes to the charts. Disabled the labels and tooltips, also set a max and min on the graphs scale.

--- public/js/irrigation.js
+++ public/js/irrigation.js
@@ -1,6 +1,9 @@
 angular.module("app", ["chart.js"])
         
-.controller("IrrigationCtrl", function ($scope, $http) {
+.controller("IrrigationCtrl", function ($scope, $http, $filter) {
+    
+    Chart.defaults.global.tooltips.enabled = false;
+    Chart.defaults.global.elements.point.radius = 0;
 
     var gray = '#DCDCDC';
     var purple = '#803690';
@@ -9,7 +12,7 @@ angular.module("app", ["chart.js"])
     var yellow = '#f0ad4e';
     var dark_blue = '#949FB1';
     var blue_gray = '#4D5360';
-
+                
     $scope.chartData = [];
     $scope.notifications = {
         data: [],
@@ -47,13 +50,20 @@ angular.module("app", ["chart.js"])
     
   
     
-    function getBlankChart(name, color){
-
+    function getBlankChart(name, color)
+    {
         return {
             labels: [],
-            data: [[], []],
-            series: [name, "5 Period MA"],
+            data: [[]], //, []],
+            series: [name], //, "5 Period MA"],
             colors: [color, blue_gray],
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {}
+                    }]
+                }
+            },
             vars: {
                 current: 0
             },
@@ -86,14 +96,20 @@ angular.module("app", ["chart.js"])
                 this.vars[count]++;
                 if(this.labels.length < this.vars[count])
                 {
-                    this.labels.push(label);
+                    var parsedLabel = $filter('date')(
+                        Date.parse(label) , 
+                        "MM/dd/yy  h:mma", 
+                        "MST"
+                    );
+            
+                    this.labels.push(parsedLabel);
                 }
             },
 
             map: function(data){
                 angular.forEach(data, function() {
                     this.addMA(data, "sum1", "num1", "count1", 1, 0);
-                    this.addMA(data, "sum5", "num5", "count5", 5, 1);
+//                    this.addMA(data, "sum5", "num5", "count5", 5, 1);
                 }, this);
                 
                 this.vars.current = data[data.length - 1]["value"];
@@ -104,12 +120,18 @@ angular.module("app", ["chart.js"])
     {
         var moisture = getBlankChart("Moisture", blue);
         moisture.map(data.moisture);
+        moisture.options.scales.yAxes[0].ticks.beginAtZero = true;
+        moisture.options.scales.yAxes[0].ticks.max = 700;
 
         var light = getBlankChart("Light", yellow);
         light.map(data.photoresistor);
+        light.options.scales.yAxes[0].ticks.beginAtZero = true;
+        light.options.scales.yAxes[0].ticks.max = 1200;
 
         var temp = getBlankChart("Temperature", green);
         temp.map(data.temp);
+        temp.options.scales.yAxes[0].ticks.beginAtZero = true;
+        temp.options.scales.yAxes[0].ticks.max = 200;
 
         return {moisture: moisture, temp: temp, light: light};
     }
