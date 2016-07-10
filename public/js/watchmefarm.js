032a722bf098023f350f2fb87298d7e489d1ec54
unknown <2.4.8.16.32.sixtyfour@gmail.com>
2016-07-10 17:04:14 -0600

    Never added my base js class...oops

--- public/js/watchmefarm.js
+++ public/js/watchmefarm.js
@@ -2,11 +2,12 @@ define([
     '../user/user',
     'charts',
     'notifications',
-    'settings',
+    'components',
+    'activities',
     'timeline',
 ],
 
-  function(user, charts, notifications, settings, timeline){
+  function(user, charts, notifications, components, activities, timeline){
       
     // Create the base module for the page
     var wmf = angular.module('watchmefarm', ['chart.js', 'ui.bootstrap', 'btorfs.multiselect']);
@@ -16,7 +17,8 @@ define([
     user.init(wmf);
     charts.init(wmf);
     notifications.init(wmf);
-    settings.init(wmf);
+    components.init(wmf);
+    activities.init(wmf);
     timeline.init(wmf);
     
     // Bootstrap the page
