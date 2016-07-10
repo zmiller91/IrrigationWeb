b20e183321f61b8a58c5a13fbeeb570f35e914ba
unknown <2.4.8.16.32.sixtyfour@gmail.com>
2016-07-10 17:16:26 -0600

    added my css files and modified the release script to pull down the correct multiselect github

--- bin/scripts/release/20160710.sh
+++ bin/scripts/release/20160710.sh
@@ -1,4 +1,4 @@
 
 cd ../../../public/lib
 git clone https://github.com/requirejs/requirejs
-git clone https://github.com/sebastianha/angular-bootstrap-multiselect
+git clone https://github.com/bentorfs/angular-bootstrap-multiselect
