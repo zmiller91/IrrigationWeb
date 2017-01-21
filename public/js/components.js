2d4b82ff1446daeb89cd0c81decae1f7fb60feeb
Zachary Miller <2.4.8.16.32.sixtyfour@gmail.com>
2017-01-21 15:16:34 -0700

    Component UI Refactor
        Added a 3 way switch to turn on, off, or do nothing with
        a component. The UI is stubbed with fake data until the
        backend is complete.

--- public/js/components.js
+++ public/js/components.js
@@ -3,7 +3,21 @@ define([], function() {
         init: function(app) {
             app.controller("ComponentsCtrl", function ($scope, ComponentStatusData) {
 
+                $scope.enabled = false;
                 $scope.components = [];
+                
+                $scope.edit = function() {
+                    $scope.enabled = true;
+                }
+                
+                $scope.cancel = function() {
+                    $scope.enabled = false;
+                }
+                
+                $scope.save = function(){
+                    $scope.enabled = false;
+                    ComponentStatusData.put(null, null, $scope.components);
+                }
                 
                 angular.element(document).ready(function() {
                     ComponentStatusData.get(function(data){
@@ -23,16 +37,58 @@ define([], function() {
                 this.response = {};
                 this.data = {};
                 
+                this.put = function(success, error, data) {
+                    console.log(data);
+                }
+                
                 this.get =  function(success,error) {
-                    $http.get('index.php', {params:{method: "component_status"}})
-                        .then(function(response) {
-                            this.response = response;
-                            this.data = response.data;
-                            success(this.data);
-                        }, function(response) {
-                            error(response);
-                        }
-                    );
+                    
+                    var data = [{
+                        "id": "1001",
+                        "name": "Food pump",
+                        "arduino_id": "2",
+                        "process": "1001",
+                        "type": "1",
+                        "state": "auto",
+                        "date": null
+                    }, {
+                        "id": "1005",
+                        "name": "Water Valve",
+                        "arduino_id": "2",
+                        "process": "1005",
+                        "type": "1",
+                        "state": "auto",
+                        "date": null
+                    }, {
+                        "id": "1006",
+                        "name": "Light",
+                        "arduino_id": "2",
+                        "process": "1006",
+                        "type": "1",
+                        "state": "off",
+                        "date": null
+                    }, {
+                        "id": "1007",
+                        "name": "Intake Fan",
+                        "arduino_id": "2",
+                        "process": "1007",
+                        "type": "1",
+                        "state": "on",
+                        "date": null
+                    }]
+                    
+                    success(data);
+                    
+                    
+//                    $http.get('index.php', {params:{method: "component_status"}})
+//                        .then(function(response) {
+//                            this.response = response;
+//                            this.data = response.data;
+//                            success(this.data);
+//                        }, function(response) {
+//                            error(response);
+//                        }
+//                    );
                 };
             }]);
         }
